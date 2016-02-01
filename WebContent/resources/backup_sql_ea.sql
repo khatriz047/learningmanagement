4af1c74d56d0cb48e87aafe4d3d7326b0f7ad7a0
tosujanmaharjan <tosujanmaharjan@localhost>
2016-01-31 22:08:25 -0600

    final sql

--- WebContent/resources/backup_sql_ea.sql
+++ WebContent/resources/backup_sql_ea.sql
@@ -77,6 +77,7 @@ DROP TABLE IF EXISTS `profile`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `profile` (
+  `address` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
   `contactNumber` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
@@ -87,13 +88,13 @@ CREATE TABLE `profile` (
   `imageUrl` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `prefix` varchar(255) DEFAULT NULL,
+  `schoolname` varchar(255) DEFAULT NULL,
   `staffInformation` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
   `studentInformation` varchar(255) DEFAULT NULL,
   `usernumber` varchar(255) DEFAULT NULL,
   `zip` varchar(255) DEFAULT NULL,
   `profile_id` bigint(20) NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`profile_id`),
   CONSTRAINT `FK_9g5vxnl1pv0gpq2wub5tjn4c2` FOREIGN KEY (`profile_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -105,7 +106,7 @@ CREATE TABLE `profile` (
 
 LOCK TABLES `profile` WRITE;
 /*!40000 ALTER TABLE `profile` DISABLE KEYS */;
-INSERT INTO `profile` VALUES ('FairField','6414513001','US','tosujanmaharjan540@gmail.co','2015-01-01','Sujan','US',NULL,'Maharjan','Mr',NULL,'IOWA',NULL,'984656','52557',1,'1000 North,4th Street');
+INSERT INTO `profile` VALUES ('1000 North,4th Street','FairField','6414513001','US','thesujan540@gmail.com',NULL,'Sujan',NULL,NULL,'Maharjan','Mr',NULL,NULL,'IOWA',NULL,NULL,'52557',1),(NULL,NULL,NULL,NULL,'asdfasdfsdf',NULL,NULL,NULL,NULL,NULL,NULL,'asdfasdf',NULL,NULL,NULL,NULL,NULL,2),(NULL,NULL,NULL,NULL,'asdfasdfsdf',NULL,NULL,NULL,NULL,NULL,NULL,'asdfasdfasdf',NULL,NULL,NULL,NULL,NULL,3),(NULL,NULL,NULL,NULL,'ASDFASDFASDF',NULL,NULL,NULL,NULL,NULL,NULL,'MUM',NULL,NULL,NULL,NULL,NULL,5),(NULL,NULL,NULL,NULL,'asdfasdfasdf',NULL,NULL,NULL,NULL,NULL,NULL,'asdfasdf',NULL,NULL,NULL,NULL,NULL,6),('none','Kathmandu','6414513001','NP','testing@testing.com',NULL,'Sujan',NULL,NULL,'Maharjan','Mr','testing',NULL,'none',NULL,NULL,'none',7),('','','','AS','school',NULL,'Sujan',NULL,NULL,'','Mr','school',NULL,'',NULL,NULL,'',8);
 /*!40000 ALTER TABLE `profile` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,10 +142,10 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,7 +154,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'ADMIN',NULL),(2,'test','test'),(3,'SCHOOL','Admin role for the user type SCHOOL');
+INSERT INTO `role` VALUES (1,'ADMIN','ADMIN'),(2,'test','SCHOOL');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,7 +204,7 @@ CREATE TABLE `user` (
   `resetRequest` bit(1) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,7 +213,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'','admin','\0','admin');
+INSERT INTO `user` VALUES (1,'','admin','\0','admin'),(2,'\0','asdfasdfasdf','\0','asdfasdfsdf'),(3,'\0','asdfasdf','\0','asdfasdf'),(5,'\0','Sujan','\0','Sujans'),(6,'\0','asdfasdfasd','\0','asdfasdfasdfasd'),(7,'\0','testing','\0','testing'),(8,'','school','\0','school');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -225,11 +226,11 @@ DROP TABLE IF EXISTS `user_role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_role` (
   `user_id` bigint(20) NOT NULL,
-  `roles_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`roles_id`),
-  UNIQUE KEY `UK_5k3dviices5fr7560hvc81x4r` (`roles_id`),
-  CONSTRAINT `FK_5k3dviices5fr7560hvc81x4r` FOREIGN KEY (`roles_id`) REFERENCES `role` (`id`),
-  CONSTRAINT `FK_apcc8lxk2xnug8377fatvbn04` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FK_it77eq964jhfqtu54081ebtio` (`role_id`),
+  CONSTRAINT `FK_apcc8lxk2xnug8377fatvbn04` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_it77eq964jhfqtu54081ebtio` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -239,7 +240,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,1);
+INSERT INTO `user_role` VALUES (1,1),(2,2),(3,2),(5,2),(6,2),(7,2),(8,2);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -252,4 +253,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-31 14:10:47
+-- Dump completed on 2016-01-31 22:07:46
